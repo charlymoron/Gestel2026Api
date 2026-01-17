d37fcc87ff9a25f2390470a4e2ee75581b894211
charlymoron <charlymoron@gmail.com>
2026-01-17 15:22:01 -0300

    Merge remote-tracking branch 'origin/main'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.cliente_router import cliente_router
+from app.api.v1.edificio_router import edificio_router
 from app.api.v1.process_routes import process_router
 from app.api.v1.provincia_router import provincia_router
 
@@ -10,10 +11,12 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
+app.include_router(cliente_router,prefix="/api/v1")
 app.include_router(process_router, prefix="/api/v1")
 app.include_router(cliente_router, prefix="/api/v1")
-
 app.include_router(provincia_router, prefix="/api/v1")
+app.include_router(edificio_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
